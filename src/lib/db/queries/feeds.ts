c3cdc6e1ff001a1c77631e10c041b40754bc150b
Tam <5542463+Tam-d@users.noreply.github.com>
2026-04-02 20:38:47 +0000

    Remove feed follow functions

--- src/lib/db/queries/feeds.ts
+++ src/lib/db/queries/feeds.ts
@@ -26,39 +26,3 @@ export async function getFeedByUrl(url: string) {
     .where(eq(feeds.url, url))
   return feed;
 }
-
-export async function createFeedFollow(
-  feedId: string, 
-  userId: string
-) {
-  const [feedFollow] = await db
-    .insert(feedFollows)
-    .values({
-      feedId: feedId,
-      userId: userId
-    })
-    .returning();
-
-  if(!feedFollow || feedFollow === null) {
-    throw Error("Failed to insert feedFollow");
-  }
-
-  const [result] = await db
-    .select()
-    .from(feedFollows)
-    .where(eq(feedFollows.id, feedFollow.id))
-    .innerJoin(feeds, eq(feeds.id, feedFollow.feedId!))
-    .innerJoin(users, eq(users.id, feedFollow.userId!));
-
-  return result;
-}
-
-export async function getFeedFollowsForUser(userId: string) {
-  const result = await db
-    .select()
-    .from(feedFollows)
-    .where(eq(feedFollows.userId, userId))
-    .innerJoin(feeds, eq(feeds.id, feedFollows.feedId))
-    .innerJoin(users, eq(users.id, feedFollows.userId));
-  return result;
-}
