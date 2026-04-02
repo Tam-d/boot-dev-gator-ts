a60a963ea5e2568b386c45236d2f09057f70e2c4
Tam <5542463+Tam-d@users.noreply.github.com>
2026-04-02 03:22:02 +0000

    Update whitespace

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -4,10 +4,18 @@ export type CommandsRegistry = {
     [key: string]: CommandHandler
 }
 
-export function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler) {
+export function registerCommand(
+    registry: CommandsRegistry, 
+    cmdName: string, 
+    handler: CommandHandler
+) {
     registry[cmdName] = handler;
 }
 
-export async function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]) {
+export async function runCommand(
+    registry: CommandsRegistry, 
+    cmdName: string, 
+    ...args: string[]
+) {
     await registry[cmdName](cmdName, ...args);
 }
