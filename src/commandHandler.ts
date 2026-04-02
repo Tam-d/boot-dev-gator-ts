633039e9f7fe2ee65bab21a078c9763a49e38b17
Tam <5542463+Tam-d@users.noreply.github.com>
2026-04-02 03:27:43 +0000

    Update whitespace

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -1,4 +1,7 @@
-export type CommandHandler = (cmdName: string, ...args: string[]) => Promise<void>;
+export type CommandHandler = (
+    cmdName: string, 
+    ...args: string[]
+) => Promise<void>;
 
 export type CommandsRegistry = {
     [key: string]: CommandHandler
