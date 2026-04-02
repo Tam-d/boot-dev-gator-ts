9306f4497d7a51085d83538e41b6fe86bdf4c845
Tam <5542463+Tam-d@users.noreply.github.com>
2026-04-02 14:10:22 +0000

    Refactor error handling in register

--- src/commands/users.ts
+++ src/commands/users.ts
@@ -28,22 +28,18 @@ export async function handlerRegister(cmdName: string, ...args: string[]) {
         throw Error("The register handler expects a single argument, a username");
     }
 
-    console.log(`Attempting to register username \"${username}\"`);
+    console.log(`Attempting to register user, \"${username}\"`);
 
-    try {
-        const createdUser = await createUser(username);
-        
-        console.log(`User created with name ${createdUser.name}`);
-        console.log(createdUser);
-        
-        setUser(createdUser.name);
+    const createdUser = await createUser(username);
+
+    if(!createdUser) {
+        throw Error("Unable to register user.");
     }
-    catch(error) {
-        console.log("There was an issue registering your user....");
-        console.log((error as Error).message);
-        console.log((error as Error).cause);
-        exit(1)
-    }
+    
+    console.log(`User created with name ${createdUser.name}`);
+    console.log(createdUser);
+    
+    setUser(createdUser.name);
 }
 
 export async function handlerGetUsers(cmdName: string, ...args: string[]) {
