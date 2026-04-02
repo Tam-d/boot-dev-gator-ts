afc83b5619fd1e7e8085502927c45e13c6392078
Tam <5542463+Tam-d@users.noreply.github.com>
2026-04-02 19:20:19 +0000

    Add queries to get feed, create feed follow,  get feed follow

--- src/lib/db/queries/feeds.ts
+++ src/lib/db/queries/feeds.ts
@@ -1,5 +1,5 @@
 import { db } from "../index.js";
-import { Feed, feeds, users } from "../schema.js";
+import { Feed, feedFollows, feeds, users } from "../schema.js";
 import { eq } from "drizzle-orm";
 
 export async function createFeed(feed: Feed) {
@@ -16,5 +16,49 @@ export async function getFeeds() {
     .from(feeds)
     .innerJoin(users, eq(feeds.userId, users.id));
 
+  return result;
+}
+
+export async function getFeedByUrl(url: string) {
+  const [feed] = await db
+    .select()
+    .from(feeds)
+    .where(eq(feeds.url, url))
+  return feed;
+}
+
+export async function createFeedFollow(
+  feedId: string, 
+  userId: string
+) {
+  const [feedFollow] = await db
+    .insert(feedFollows)
+    .values({
+      feedId: feedId,
+      userId: userId
+    })
+    .returning();
+
+  if(!feedFollow || feedFollow === null) {
+    throw Error("Failed to insert feedFollow");
+  }
+
+  const [result] = await db
+    .select()
+    .from(feedFollows)
+    .where(eq(feedFollows.id, feedFollow.id))
+    .innerJoin(feeds, eq(feeds.id, feedFollow.feedId!))
+    .innerJoin(users, eq(users.id, feedFollow.userId!));
+
+  return result;
+}
+
+export async function getFeedFollowsForUser(userId: string) {
+  const result = await db
+    .select()
+    .from(feedFollows)
+    .where(eq(feedFollows.userId, userId))
+    .innerJoin(feeds, eq(feeds.id, feedFollows.feedId))
+    .innerJoin(users, eq(users.id, feedFollows.userId));
   return result;
 }
