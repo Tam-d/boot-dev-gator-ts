55a10e8086672aba10335a873655c8be855f119d
Tam <5542463+Tam-d@users.noreply.github.com>
2026-04-02 03:25:26 +0000

    Register commands after parsing args

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,21 @@ import { handlerReset } from "./commands/reset.js";
 import { handlerAddFeed, handlerAggregate } from "./commands/feed.js";
 
 async function main() {
-    const registry : CommandsRegistry = {};
-
-    registerCommand(registry, "login", handlerLogin);
-    registerCommand(registry, "register", handlerRegister);
-    registerCommand(registry, "reset", handlerReset);
-    registerCommand(registry, "users", handlerGetUsers);
-    registerCommand(registry, "agg", handlerAggregate);
-    registerCommand(registry, "addfeed", handlerAddFeed);
-
+    
     const args = process.argv.slice(2);
 
     if( args.length < 1) {
         console.log("incorrect number of arguments");
         exit(1);
     }
+    
+    const registry : CommandsRegistry = {};
+    registerCommand(registry, "login", handlerLogin);
+    registerCommand(registry, "register", handlerRegister);
+    registerCommand(registry, "reset", handlerReset);
+    registerCommand(registry, "users", handlerGetUsers);
+    registerCommand(registry, "agg", handlerAggregate);
+    registerCommand(registry, "addfeed", handlerAddFeed);
 
     const command = args[0];
     const commandArgs = args.slice(1);
